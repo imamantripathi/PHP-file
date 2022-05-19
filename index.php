e42ec3ce725b6190332d9acc2ff3f868cd9a369b
Aman Tripathi <imamantripathi@gmail.com>
2022-05-19 11:20:02 +0530

    !

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a=7;
+$b=6;
+$c=8;
+$d=8;
+$e=6;
+$sum=$a+$b+$c+$d+$e;
+$percentage=($sum/500)*100;
+echo("Subject 1 is: $a <br>");
+echo("Subject 2 is: $b <br>");
+echo("Subject 3 is: $c <br>");
+echo("Subject 4 is: $d <br>");
+echo("Subject 5 is: $e <br>");
+echo("Sum of subjects is: $sum <br>");
+echo("Percentage of subjects is: $percentage"."%");
+?>
+</body>
+</html>
